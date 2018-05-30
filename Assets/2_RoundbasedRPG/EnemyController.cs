187b22101d6fd306fa16135e569655982533bb42
Zyntos <zyrielsleepwalker@gmail.com>
2018-05-30 15:12:38 +0200

    Function RPG

---
+++ Assets/2_RoundbasedRPG/EnemyController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyController : MonoBehaviour {
+
+    public int enemyhealth = 100;
+    public Slider enemyHealthSlider;
+    public bool enemyturn = false;
+    public bool playerturn = true;
+    public GameObject player;
+
+    // Use this for initialization
+    void Start() {
+        enemyHealthSlider.maxValue = 100;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        enemyHealthSlider.value = enemyhealth;
+
+        if (enemyturn)
+        {
+            StartCoroutine(EnemyAttack());
+            enemyturn = false;
+
+        }
+    }
+
+    private IEnumerator EnemyAttack()
+    {
+        yield return new WaitForSeconds(3);
+        player.GetComponent<PlayerController>().getDamaged(20);
+        
+        playerturn = true;
+
+
+    }
+   
+
+    public void getDamaged(int damage)
+    {
+        enemyhealth = enemyhealth - damage;
+    }
+
+   
+    
+
+
+}
---
+++ Assets/2_RoundbasedRPG/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    public int health = 100;
+    public Slider playerHealth;
+
+	// Use this for initialization
+	void Start () {
+        playerHealth.maxValue = 100;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        playerHealth.value = health;
+	}
+
+    public void getDamaged(int damage)
+    {
+        health = health - damage;
+    }
+
+    public void getHealed(int damage)
+    {
+        health = health + damage;
+    }
+}
