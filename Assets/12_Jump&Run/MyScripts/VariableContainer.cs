fdcae48c1457add87de3484d7b838ee6bdad07ab
McMuffinman <steinmetzmarvin@gmx.de>
2018-06-26 19:20:14 +0200

    Integration Gruppe 12 Jump n Run

---
+++ Assets/12_Jump&Run/MyScripts/VariableContainer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariableContainer : MonoBehaviour {
+
+    public int attempts=2;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+
+        
+        
+    }
+
+    // Use this for initialization
+    void Start() {
+    }
+
+  
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
