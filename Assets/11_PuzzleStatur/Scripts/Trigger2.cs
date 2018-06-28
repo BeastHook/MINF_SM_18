116c441d61fc5ba76f6ceb5e5f3636ffe8b1bda6
Unknown <inga.fuhrmann@gmail.com>
2018-06-28 19:33:45 +0200

    11_PuzzleStatur

---
+++ Assets/11_PuzzleStatur/Scripts/Trigger2.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger2 : MonoBehaviour
+{
+ 
+
+    
+    public GameObject Vorderbeine;
+    public GameObject Mitte;
+
+    public AudioSource source;
+    public AudioClip audioC;
+    public bool played = false;
+
+
+    public GameObject Plane5;
+    public GameObject Plane6;
+
+    void Start()
+    {
+      source=gameObject.GetComponent<AudioSource> ();
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("rechts5")&&(!played))
+        {
+            Debug.Log("True");
+           
+            source.PlayOneShot(audioC);
+            played = true;
+
+            Mitte.transform.parent = Vorderbeine.transform;
+            Plane5.transform.parent = Plane6.transform;
+            Mitte.gameObject.tag = "loesung";
+            Vorderbeine.gameObject.tag = "loesung";
+        
+        }
+    }
+
+}
---
+++ Assets/11_PuzzleStatur/Scripts/Trigger3.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger3 : MonoBehaviour {
+  //  public Transform Group2;
+    public GameObject Kopf;
+    public GameObject KörperFlügel;
+    
+    public AudioSource source;
+    public AudioClip audioC;
+    public bool played = false;
+
+    public GameObject Plane3;
+    public GameObject Plane2;
+
+    void Start()
+    {
+        source = gameObject.GetComponent<AudioSource>();    
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("rechts2")&&(!played))
+        {
+            Debug.Log("True");
+
+            source.PlayOneShot(audioC);
+            played = true;
+
+            //  Mitte.transform.SetParent(Group4, false);
+            KörperFlügel.transform.parent = Kopf.transform;
+            Plane2.transform.parent = Plane3.transform;
+            Kopf.gameObject.tag = "loesung";
+            KörperFlügel.gameObject.tag = "loesung";
+          
+        }
+    }
+}
