56cec17aa8bb1af836b604202c1643c3269f6d34
WolfEngeland <wolfengeland@hotmail.com>
2018-07-09 16:17:41 +0200

    Cupboy Final
    Endbedingung im ChestScript in der Collision

---
+++ Assets/3_Cupboy/CupBoyAssets/Scripts/SidePlatformParent.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SidePlatformParent : MonoBehaviour {
+
+    public Transform platform;
+    public Transform imagetarget;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("CupBoy")) {
+            collision.collider.transform.SetParent(platform);
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.tag.Equals("CupBoy")) {
+            collision.collider.transform.SetParent(imagetarget);
+        }
+    }
+    
+   
+}
