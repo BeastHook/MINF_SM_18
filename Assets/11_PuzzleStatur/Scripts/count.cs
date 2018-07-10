0bded44f15cf047f7c756086bc95de82a84635f4
Luca <ukrowd@yahoo.de>
2018-07-10 12:18:21 +0200

    PuzzleStatur End unload fix

--- Assets/11_PuzzleStatur/Scripts/count.cs
+++ Assets/11_PuzzleStatur/Scripts/count.cs
@@ -106,8 +106,7 @@ public class count : MonoBehaviour {
         Pferdekopf.SetActive(false);
         RENOMME.SetActive(true);
 
-	    MultisceneManager.Instance.StartCoroutine(MultisceneManager.Instance.FinishLevel(true));
-	}
+    }
     void boxOpen()
     {
         anim.GetComponent<Animation>().Play();
